3497fccfa9854d4f60b7c1527eb1b103e5aed414
Lobashik <74973413+Lobashik@users.noreply.github.com>
2021-03-26 17:29:35 +0300

    Delete true.py

--- true.py
+++
@@ -1,9 +0,0 @@
-  def check_new_password(password): # проверяет подходит ли пароль пользователя требованиям системы
-    if len(password) < 9:
-        raise PasswordError('Пароль должен быть длиной более 8 символов')
-    if password.islower() or password.isupper() or not any(map(str.isalpha, password)):
-        raise PasswordError('В пароле долны быть хоят бы одна буква в верхнем регисте и одна в маленьком')
-    if not any(map(str.isdigit, password)):
-        raise PasswordError('В пароле должна быть хотя бы одна цифра')
-    if sequence(password.lower()):
-        raise PasswordError('В пароле не должно быть 3 идущих подряд на клавиатуре символа')
